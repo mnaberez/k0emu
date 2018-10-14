58fdb242b4c98c8bf288185b046a73023fb9546c
Mike Naberezny <mike@naberezny.com>
2018-10-14 12:22:47 -0700

    Add NOT1 CY

--- processor.py
+++ processor.py
@@ -10,13 +10,35 @@ class Processor(object):
 
     def step(self):
         opcode = self.memory[self.pc]
-        if opcode == 0: # nop
+        # nop
+        if opcode == 0x00:
             self.pc += 1
-        elif opcode & 0b11111000 == 0b10100000: # mov r,#byte
+
+        # not1 cy
+        elif opcode == 0x01:
+            bitweight = Flags.CY
+            carry = self.psw & bitweight
+            if carry:
+                self.psw &= ~bitweight
+            else:
+                self.psw |= bitweight
+            self.pc += 1
+
+        # movw ax,0fe20h              ;02 CE AB       addr16p
+        elif opcode == 0x02:
+            addr16p = _addr16p(self.memory[self.pc+1],
+                               self.memory[self.pc+2])
+            data = self.memory[addr16p]
+            self._op_movw(addr16p, data)
+            # TODO finish me
+
+        # mov r,#byte
+        elif opcode & 0b11111000 == 0b10100000:
             regnum = opcode & 0b00000111
             immbyte = self.memory[self.pc+1]
             self._op_mov(regnum, immbyte)
             self.pc += 2
+
         elif opcode & 0b01100001:
             opcode2 = self.memory[self.pc+1]
             if opcode2 & 0b1100111 == 0b1100000: # sel rbn
@@ -29,6 +51,9 @@ class Processor(object):
 
     def _op_mov(self, regnum, data):
         self.write_gp_reg(regnum, data)
+
+    def _op_movw(self, regpairnum, data):
+        pass
 
     def read_gp_reg(self, regnum):
         address = self.address_of_gp_reg(regnum)
@@ -51,11 +76,33 @@ class Processor(object):
         for address, value in enumerate(data, address):
             self.memory[address] = value
 
-Registers = {'X': 0,
-             'A': 1,
-             'C': 2,
-             'B': 3,
-             'E': 4,
-             'D': 5,
-             'L': 6,
-             'H': 7}
+def _addr16p(low, high):
+    addr16p = low + (high << 8)
+    if addr16p & 1 != 0:
+        raise Exception("addr16p must be an even address")
+    return addr16p
+
+class Registers(object):
+    X = 0
+    A = 1
+    C = 2
+    B = 3
+    D = 5
+    L = 6
+    H = 7
+
+class RegisterPairs(object):
+    AX = 0
+    BC = 1
+    DE = 2
+    HL = 3
+
+class Flags(object):
+    CY     = 2**0
+    ISP    = 2**1
+    UNUSED = 2**2
+    RBS0   = 2**3
+    AC     = 2**4
+    RBS1   = 2**5
+    Z      = 2**6
+    IE     = 2**7
--- test_processor.py
+++ test_processor.py
@@ -1,14 +1,34 @@
 import unittest
 import sys
-from processor import Processor, Registers
+from processor import Processor, Registers, Flags
 
 class ProcessorTests(unittest.TestCase):
     # nop
     def test_nop(self):
         proc = Processor()
-        code = [0] # nop
+        code = [0x00] # nop
         proc.write_memory(0x0000, code)
         proc.step()
+        self.assertEqual(proc.pc, len(code))
+
+    # not1 cy
+    def test_not1_cy_0_to_1(self):
+        proc = Processor()
+        code = [0x01] # not1 cy
+        proc.write_memory(0x0000, code)
+        proc.psw = 0
+        proc.step()
+        self.assertEqual(proc.psw, Flags.CY)
+        self.assertEqual(proc.pc, len(code))
+
+    # not1 cy
+    def test_not1_cy_1_to_0(self):
+        proc = Processor()
+        code = [0x01] # not1 cy
+        proc.write_memory(0x0000, code)
+        proc.psw = 0xFF
+        proc.step()
+        self.assertEqual(proc.psw, 0xFF & ~Flags.CY)
         self.assertEqual(proc.pc, len(code))
 
     # mov r,#byte
@@ -17,7 +37,7 @@ class ProcessorTests(unittest.TestCase):
         code = [0xA0, 0x42] # mov x, #42
         proc.write_memory(0x0000, code)
         proc.step()
-        self.assertEqual(proc.read_gp_reg(Registers['X']), 0x42)
+        self.assertEqual(proc.read_gp_reg(Registers.X), 0x42)
         self.assertEqual(proc.pc, len(code))
 
     # mov r,#byte
@@ -26,7 +46,7 @@ class ProcessorTests(unittest.TestCase):
         code = [0xA7, 0x42] # mov h, #42
         proc.write_memory(0x0000, code)
         proc.step()
-        self.assertEqual(proc.read_gp_reg(Registers['H']), 0x42)
+        self.assertEqual(proc.read_gp_reg(Registers.H), 0x42)
         self.assertEqual(proc.pc, len(code))
 
 
