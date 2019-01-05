07c80228424d642ab71d20857c2eaff5c8ec5589
Mike Naberezny <mike@naberezny.com>
2019-01-04 16:06:28 -0800

    Hack in memory dump on exit

--- k0emu/run.py
+++ k0emu/run.py
@@ -16,22 +16,32 @@ def main():
             rom = bytearray(f.read())
         proc.write_memory(0, rom)
         proc.reset()
+        for address in range(0xfb00, 0x10000):
+            proc.memory[address] = 0x17
     else:
         sys.stderr.write(__doc__)
         sys.exit(1)
 
     while True:
-        dasm = disassemble(proc.memory, proc.pc)
-        hex = ' '.join(["%02x" % x for x in dasm.all_bytes]).ljust(12)
-        line = ("%04x: %s %s" % (proc.pc, hex, dasm)).ljust(36)
         try:
-            proc.step()
-        except NotImplementedError:
-            line += "!!! NOT IMPLEMENTED !!!"
+            dasm = disassemble(proc.memory, proc.pc)
+            hex = ' '.join(["%02x" % x for x in dasm.all_bytes]).ljust(12)
+            line = ("%04x: %s %s" % (proc.pc, hex, dasm)).ljust(36)
+            try:
+                proc.step()
+            except NotImplementedError:
+                line += "!!! NOT IMPLEMENTED !!!"
+                print(line)
+                break
+            line += str(proc)
             print(line)
-            sys.exit(1)
-        line += str(proc)
-        print(line)
+        except KeyboardInterrupt:
+            break
+
+    filename = "/Users/mnaberez/Desktop/memory.bin"
+    with open(filename, 'wb') as f:
+        f.write(proc.memory)
+    print("\nDump written to: %s" % filename)
 
 if __name__ == "__main__":
     main()
