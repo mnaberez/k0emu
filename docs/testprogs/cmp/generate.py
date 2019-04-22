f8c1dc51263b668125b4f19b104a2e9279e9327e
Mike Naberezny <mike@naberezny.com>
2019-04-22 11:52:18 -0700

    Remove obsolete test programs

--- docs/testprogs/cmp/generate.py
+++
@@ -1,9 +0,0 @@
-for a in range(0, 0x100):
-  for x in range(0, 0x100):
-    print("    ;A=%02X, X=%02X" % (a, x))
-    print("    mov a,#0x%02x" % x)
-    print("    mov x,a")
-    print("    mov a,#0x%02x" % a)
-    print("    cmp a,x")
-    print("    call print")
-    print("    ")
