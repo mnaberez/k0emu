c7b40a5078144cd49d0a06baaa7b1b13ed7295a5
Mike Naberezny <mike@naberezny.com>
2019-04-22 20:56:33 -0700

    Add tests for ROL, ROR, ROLC, RORC

---
+++ k0emu/debugger/testprogs/fixtures/test_rol.py
@@ -0,0 +1,30 @@
+import sys
+from k0emu.debug import make_debugger_from_argv
+
+def test(debug, outfile):
+    for psw in (0, 1):
+        for a in range(256):
+            code = [
+                0xA1, psw,           # mov a,#<psw>
+                0xF2, 0x1E,          # mov psw,a
+                0xA1, a,             # mov a,#<a>
+                0x26,                # rol a,1
+                0x9E, 0x00, 0xFE,    # mov 0xfe00,a
+                0xF0, 0x1E,          # mov a,psw
+                0x9E, 0x01, 0xFE,    # mov 0xfe01,a
+                0xAF,                # ret
+            ]
+            debug.write(0xf000, code)
+            debug.branch(0xf000)
+            a_out, psw_out = debug.read(0xfe00, length=2)
+
+            fmt = "A(IN)=%02x, PSW(IN)=%02x -> A(OUT)=%02x, PSW(OUT)=%02x\n"
+            outfile.write(fmt % (a, psw, a_out, psw_out))
+            outfile.flush()
+
+def main():
+    debug = make_debugger_from_argv()
+    test(debug, sys.stdout)
+
+if __name__ == '__main__':
+    main()
