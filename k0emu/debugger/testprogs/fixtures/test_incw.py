23d30025c3df7c442e07a2ad80ae89447f4e54cc
Mike Naberezny <mike@naberezny.com>
2019-04-26 17:12:37 -0700

    Add test for INCW

---
+++ k0emu/debugger/testprogs/fixtures/test_incw.py
@@ -0,0 +1,35 @@
+import itertools
+import sys
+from k0emu.debug import make_debugger_from_argv
+
+def test(debug, outfile):
+    low  = lambda word: word & 0xff
+    high = lambda word: word >> 8
+
+    psw_iterator = itertools.cycle((0x00, 0x01, 0x10, 0x40, 0x51))
+    for ax in range(0x10000):
+        psw = next(psw_iterator)
+        code = [
+            0x11, 0x1e, psw,            # mov psw,#<psw>
+            0x10, low(ax), high(ax),    # movw ax,#<ax>
+            0x80,                       # incw ax
+            0x03, 0x06, 0xfe,           # movw 0xfe06,ax
+            0xf0, 0x1e,                 # mov a,psw
+            0x9e, 0x08, 0xfe,           # mov 0xfe08,a
+            0xaf                        # ret
+        ]
+        debug.write(0xf000, code)
+        debug.branch(0xf000)
+        x_out, a_out, psw_out = debug.read(0xfe06, length=3)
+        ax_out = (a_out << 8) + x_out
+
+        fmt = "AX(IN)=%04x, PSW(IN)=%02x -> AX(OUT)=%04x, PSW(OUT)=%02x\n"
+        outfile.write(fmt % (ax, psw, ax_out, psw_out))
+        outfile.flush()
+
+def main():
+    debug = make_debugger_from_argv()
+    test(debug, sys.stdout)
+
+if __name__ == '__main__':
+    main()
